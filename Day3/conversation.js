31e88429bdeea99cc3d8e2f655eb69421846e1f3
Ramajan Tahashildar <154545514+ramajan-tahashildar@users.noreply.github.com>
2024-09-09 23:26:23 +0530

    Add files via upload

---
+++ Day3/conversation.js
@@ -0,0 +1,22 @@
+let score= 33
+console.log(typeof score);
+ 
+let numberasstring = true
+console.log(numberasstring);
+console.log(typeof numberasstring);
+
+/* to covert data formate*/
+let dataconversation= Number(numberasstring)
+console.log(dataconversation);
+console.log(typeof dataconversation);
+
+/* to convert  string*/
+console.log("start");
+let number=258369
+console.log(typeof  number);
+let convert= String(number)
+console.log( typeof convert);
+
+
+
+
---
+++ Day4/operations.js
@@ -0,0 +1,36 @@
+let value=33
+let negvalue= -value
+console.log(negvalue);
+
+// operations
+console.log(2+2);
+console.log(2-6);
+console.log(8*3);
+console.log(8**3); //power
+console.log(4/3);
+console.log(12%5);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("2"+2+6);
+console.log(2+2+"6");
+
+console.log((3+4)*5%2);   //use () increase code quality
+
+
+console.log(+""); //output: 0
+
+
+let name1, name2, name3
+name1="Ramazan"
+name2="heena"
+name3="dad"
+
+console.table([name1,name2,name3])
+
+//increment number
+let gamecounter=20
+// gamecounter++;
+++gamecounter;
+console.log(gamecounter);
